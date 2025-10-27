503f91d4019cbc9fac31e0405c56c1b20259773b
AndrewLeoR <andleo30@gmail.com>
2025-10-26 23:54:20 -0600

    Test exitoso para toda la gramatica en general

--- patito-entrega-1/main.py
+++ patito-entrega-1/main.py
@@ -3,14 +3,19 @@ from patito_grammar import grammar
 
 def main():
     parser = Lark(grammar, start="start", parser="lalr")
-    test_factor = """
-        program FactorTest;
-        main { 
-            x = (1 + 2 * 3); 
-            y = (a); 
-        } end
+    test_genral = """
+    program TestGeneral;
+    var x,y:int;
+    void util(p:int)[ { print(p); } ];
+    main {
+        x = 10;
+        y = x + 1 - 2 * 3;
+        if ( y < 0 ) { print("neg", y); } else { util(y); } ;
+        while ( y != 0 ) do { y = y - 1; } ;
+        print("done", x, y);
+    } end
     """
-    tree = parser.parse(test_factor)
+    tree = parser.parse(test_genral)
 
     print(tree.pretty())
 
