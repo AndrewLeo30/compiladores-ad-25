c7a0f0c00f89f07b06ecc83ed5d88e125168b86a
AndrewLeoR <andleo30@gmail.com>
2025-10-26 23:41:41 -0600

    Test exitoso para <TERMINO>

--- patito-entrega-1/main.py
+++ patito-entrega-1/main.py
@@ -3,13 +3,13 @@ from patito_grammar import grammar
 
 def main():
     parser = Lark(grammar, start="start", parser="lalr")
-    test_exp = """
-        program Exp;
+    test_term = """
+        program TermTest;
         main {
-        y = (1 + 2) - (3 - 4);
+        y = (1 + 2) * (3 - 4) / (5 - 1);
         } end
     """
-    tree = parser.parse(test_exp)
+    tree = parser.parse(test_term)
 
     print(tree.pretty())
 
